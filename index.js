c4d2e37c15f2405f2b55b07c1ce9eeb3751db972
Alexandre Beaurain <alexandre.beaurain@adfab.fr>
2017-11-02 15:03:00 +0100

    fix: format & remove unused dependencies

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import {
   DeviceEventEmitter,
   NativeModules
@@ -7,41 +6,42 @@ import {
 var RNPedometer = NativeModules.BMDPedometer;
 
 var Pedometer = {
+
   isStepCountingAvailable: function(callback) {
-        RNPedometer.isStepCountingAvailable(callback);
-      },
+    RNPedometer.isStepCountingAvailable(callback);
+  },
 
   isDistanceAvailable: function(callback) {
-        RNPedometer.isDistanceAvailable(callback);
-      },
+    RNPedometer.isDistanceAvailable(callback);
+  },
 
   isFloorCountingAvailable: function(callback) {
-        RNPedometer.isFloorCountingAvailable(callback);
-      },
+    RNPedometer.isFloorCountingAvailable(callback);
+  },
 
   isPaceAvailable: function(callback) {
-        RNPedometer.isPaceAvailable(callback);
-      },
+    RNPedometer.isPaceAvailable(callback);
+  },
 
   isCadenceAvailable: function(callback) {
-        RNPedometer.isCadenceAvailable(callback);
-      },
+    RNPedometer.isCadenceAvailable(callback);
+  },
 
   startPedometerUpdatesFromDate: function(date, handler) {
-        RNPedometer.startPedometerUpdatesFromDate(date);
+    RNPedometer.startPedometerUpdatesFromDate(date);
     DeviceEventEmitter.addListener(
-            'pedometerDataDidUpdate',
-            handler
-          );
-      },
+      'pedometerDataDidUpdate',
+       handler
+    );
+  },
 
   queryPedometerDataBetweenDates: function(startDate, endDate, handler) {
-        RNPedometer.queryPedometerDataBetweenDates(startDate, endDate, handler);
-      },
+    RNPedometer.queryPedometerDataBetweenDates(startDate, endDate, handler);
+  },
 
   stopPedometerUpdates: function () {
-        RNPedometer.stopPedometerUpdates();
-      }
+    RNPedometer.stopPedometerUpdates();
+  }
 
 };
 
